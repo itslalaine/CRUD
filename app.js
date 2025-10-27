dbe1cf075f8c9d537bd1ddc8b0ac138f90ca8a31
lalainemadrona <lalainemina.madrona@bicol-u.edu.ph>
2025-10-27 21:36:03 +0800

    Add .gitignore and remove node_modules from tracking

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require ('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
